d16baa8b67022b14a619961ed2d8d822c1a5fa1d
Yash-Terekar <yashterekar26@gmail.com>
2026-08-11 21:33:30 +0530

    JavaScript in JSX

--- src/fundamentals/jsx/practice/02-javascript-in-jsx/UserInfo.jsx
+++ src/fundamentals/jsx/practice/02-javascript-in-jsx/UserInfo.jsx
@@ -4,13 +4,13 @@ const UserInfo = () => {
   const name = "Yash Manoj Terekar";
   const age = 23;
   const course = "MCA";
-  const isLearningReact = "Yes";
+  const isLearningReact = true;
   return (
     <div>
       <h1>Name: {name}</h1>
       <h1>Age: {age}</h1>
       <h1>Course: {course}</h1>
-      <h1>Learning React: {isLearningReact}</h1>
+      <h1>Learning React: {isLearningReact ? "Yes" : "No"}</h1>
     </div>
   );
 };
