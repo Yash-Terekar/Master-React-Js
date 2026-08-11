5459c6c39d73e36fa6bd68232e93649a8738e913
Yash-Terekar <yashterekar26@gmail.com>
2026-08-11 14:17:55 +0530

    Basics of react

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
 import React, { useState } from "react";
+import Basic from "./fundamentals/jsx/practice/01-basic-jsx/Basic";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  const increament = () => setCount(count + 1);
-  // const decreament = () => setCount(count - 1);
-
   return (
-    <div
-      style={{
-        display: "flex",
-        width: "100%",
-        height: "100vh",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      {/* <button onClick={decreament}>+</button> */}
-
-      <button
-        onClick={increament}
-        style={{
-          padding: "10px 20px",
-          backgroundColor: "greenyellow",
-          border: "2px solid green",
-          borderRadius: "10px",
-        }}
-      >
-        Count {count}
-      </button>
+    <div>
+      <Basic />
     </div>
   );
 };
---
+++ src/fundamentals/jsx/practice/01-basic-jsx/Basic.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Basic = () => {
+  return (
+    <div>
+      <h1>Yash Terekar</h1>
+      <h2>MCA</h2>
+      <h2>
+        Zeal Institute of Buisness Addminstration and Computer Application
+      </h2>
+      <h2>I am learning React.</h2>
+    </div>
+  );
+};
+
+export default Basic;
