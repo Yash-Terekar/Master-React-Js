31024fda591b6b39f95ca0f61dea83a6888974bb
Yash-Terekar <yashterekar26@gmail.com>
2026-08-11 21:27:41 +0530

    JSX Rules

---
+++ src/fundamentals/jsx/practice/03-jsx-rules/Rules.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Rules = () => {
+  return (
+    <>
+      <div className="container">
+        <img src="https://avatars.githubusercontent.com/u/194201875?v=4" />
+        <h1>10 + 20 : {10 + 20}</h1>
+      </div>
+    </>
+  );
+};
+
+export default Rules;
