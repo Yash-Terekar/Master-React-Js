dc7cefd465cf0366ecd715e36fa4c637db7e4a6e
Yash-Terekar <yashterekar26@gmail.com>
2026-08-12 00:11:35 +0530

    Render Multiple Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import Header from "./fundamentals/components/practice/02-multiple-components/Header";
 import MainContent from "./fundamentals/components/practice/02-multiple-components/MainContent";
 import Footer from "./fundamentals/components/practice/02-multiple-components/Footer";
+import Header from "./fundamentals/components/practice/02-multiple-components/Header";
 
 const App = () => {
   return (
-    <div style={{ margin: "0", padding: "0" }}>
+    <div>
       <Header />
       <MainContent />
       <Footer />
