29647bf9014c8a22dfe5fed85e4fcd040ce70322
Yash-Terekar <yashterekar26@gmail.com>
2026-08-17 23:14:06 +0530

    App.js updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
-import ProfilePage from "./fundamentals/events/challenges/01-interactive-profile/ProfilePage";
-import LoginForm from "./fundamentals/events/challenges/02-login-form/LoginForm";
-
-import Products from "./fundamentals/events/challenges/03-product-actions/Products";
-
 const App = () => {
-  return (
-    <div>
-      <ProfilePage />
-      <LoginForm />
-      <Products />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
