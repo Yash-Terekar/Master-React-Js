377c8e5b0e857de68ccb8df2f32959e2696a68c8
Yash-Terekar <yashterekar26@gmail.com>
2026-08-19 23:08:46 +0530

    App added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Message from "./fundamentals/state/practice/01-basic-state/Message";
+import Counter from "./fundamentals/state/practice/02-counter/Counter.jsx";
+import Toggle from "./fundamentals/state/practice/03-toggle/Toggle.jsx";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Message />
+      <Counter />
+      <Toggle />
+    </div>
+  );
 };
 
 export default App;
