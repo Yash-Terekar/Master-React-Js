7d2d86835fe0b340a16f0c96bf9012aa58ae361f
Yash-Terekar <yashterekar26@gmail.com>
2026-08-20 23:50:13 +0530

    State Update Realtime

--- src/fundamentals/state/practice/05-multiple-state/UserProfile.jsx
+++ src/fundamentals/state/practice/05-multiple-state/UserProfile.jsx
@@ -2,9 +2,20 @@ import { useState } from "react";
 
 const UserProfile = () => {
   const [name, setName] = useState("Yash");
+  const [age, setAge] = useState(26);
+
+  const changeName = () => {
+    setName("Yash Terekar");
+  };
+  const changeAge = () => {
+    setAge(23);
+  };
   return (
     <div>
       <h1>Name {name}</h1>
+      <h1>Age {age}</h1>
+      <button onClick={changeName}>Change Name</button>
+      <button onClick={changeAge}>Increase Age</button>
     </div>
   );
 };
