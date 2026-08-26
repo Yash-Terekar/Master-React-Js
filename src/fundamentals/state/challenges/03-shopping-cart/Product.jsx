5badc08a979f9b3364a97bd9c7a251e668f85bc3
Yash-Terekar <yashterekar26@gmail.com>
2026-08-26 23:36:59 +0530

    Shopping Cart App

--- src/fundamentals/state/challenges/03-shopping-cart/Product.jsx
+++ src/fundamentals/state/challenges/03-shopping-cart/Product.jsx
@@ -1,4 +1,10 @@
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({ name, price, clicked }) => {
+  return (
+    <div>
+      <h1>Product Name : {name}</h1>
+      <h2>Price ${price}</h2>
+      <button onClick={clicked}>Add To Cart</button>
+    </div>
+  );
 };
 export default Product;
