ad9b5f46b2143059f1666112382452ac3be7da92
Yash-Terekar <yashterekar26@gmail.com>
2026-08-27 23:42:05 +0530

    Shopping Cart App

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
       <Skills />
       <Counter /> */}
       {/* <CounterApp /> */}
-      {/* <ProfileToggle /> */}
+      <ProfileToggle />
       <Shop />
     </div>
   );
