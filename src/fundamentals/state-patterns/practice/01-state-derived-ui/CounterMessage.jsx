e2dd03d325b835d7c7267a0a3cafa7e18e3a1e12
Yash-Terekar <yashterekar26@gmail.com>
2026-08-30 23:42:40 +0530

    State with Props Starts

--- src/fundamentals/state-patterns/practice/01-state-derived-ui/CounterMessage.jsx
+++ src/fundamentals/state-patterns/practice/01-state-derived-ui/CounterMessage.jsx
@@ -2,6 +2,25 @@ import { useState } from "react";
 
 const CounterMessage = () => {
   const [count, setCount] = useState(0);
-  return <div>Count {count}</div>;
+  const decrease = () => {
+    setCount(count - 1);
+  };
+  const increase = () => {
+    setCount(count + 1);
+  };
+  let status = "Starting";
+  if (count < 0) {
+    status = "Negative";
+  } else if (count > 0) {
+    status = "Positive";
+  }
+  return (
+    <div>
+      <h1> Count {count}</h1>
+      <button onClick={decrease}>Decrease</button>
+      <button onClick={increase}>Increase</button>
+      <h1>Status {status}</h1>
+    </div>
+  );
 };
 export default CounterMessage;
