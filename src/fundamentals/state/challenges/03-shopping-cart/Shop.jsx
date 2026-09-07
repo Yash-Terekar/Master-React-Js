0476428ee779fb622d10f84ca02d8365b0717e8c
Yash-Terekar <yashterekar26@gmail.com>
2026-09-07 23:42:00 +0530

    Shopping Cart State Management

--- src/fundamentals/state/challenges/03-shopping-cart/Shop.jsx
+++ src/fundamentals/state/challenges/03-shopping-cart/Shop.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import Cart from "./Cart";
 import Product from "./Product";
 
 const Shop = () => {
+  const [count, setCount] = useState(0);
+  const clickedBtn = () => setCount(count + 1);
   return (
     <div>
-      <Cart />
-      <Product name="Asus Strix G16" price={160000} />
-      <Product name="Iphone 17" price={80000} />
-      <Product name="Mouse" price={2600} isClicked />
+      <Cart count={count} />
+      <Product name="Asus Strix G16" price={160000} clicked={clickedBtn} />
+      <Product name="Iphone 17" price={80000} clicked={clickedBtn} />
+      <Product name="Mouse" price={2600} clicked={clickedBtn} />
     </div>
   );
 };
